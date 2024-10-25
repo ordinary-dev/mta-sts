08e5e9e4e2c774993c913f68a1bb6cba7e97b88e
Ivan Reshetnikov <ordinarydev@protonmail.com>
2024-10-25 15:40:39 +0500

    Remove unnecessary comments, rename the request handler

--- main.go
+++ main.go
@@ -16,22 +16,17 @@ func getVar(key string, fallback string) string {
 	return value
 }
 
-func getMtaSts(w http.ResponseWriter, r *http.Request) {
-	// Headers
+func handleRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 
-	// STS Version
 	io.WriteString(w, "version: STSv1\n")
 
-	// Mode
 	mode := getVar("MTA_STS_MODE", "enforce")
 	io.WriteString(w, "mode: "+mode+"\n")
 
-	// Max age
 	maxAge := getVar("MTA_STS_MAX_AGE", "604800")
 	io.WriteString(w, "max_age: "+maxAge+"\n")
 
-	// MX servers
 	mxServers := strings.Split(getVar("MTA_STS_MX", "mx.change.me"), ",")
 	for _, server := range mxServers {
 		io.WriteString(w, "mx: "+server+"\n")
@@ -41,7 +36,7 @@ func getMtaSts(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := getVar("PORT", "8080")
 	log.Println("Starting server at localhost:" + port)
-	http.HandleFunc("/.well-known/mta-sts.txt", getMtaSts)
+	http.HandleFunc("/.well-known/mta-sts.txt", handleRequest)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
